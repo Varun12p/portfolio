5a47993b1fdb9a0d7b9b0594bc6984fb1e8b4451
Varun <panjwanivarun12@gmail.com>
2024-08-19 16:42:26 -0400

    Day22: Render rows using import pandas package

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 col1 , col2 = st.columns(2)
@@ -15,3 +16,23 @@ with col2:
 information = """Below are all the apps I have developed and you can also find the links to
 it. I am also writing a brief description of my apps."""
 st.write(information)
+
+col3, empty_col, col4 = st.columns([1.5,0.5,1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+ row["image"])
+        st.write(f"[Source link]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+ row["image"])
+        st.write(f"[Source link]({row['url']})")
+
+
